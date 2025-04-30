9dffc847a4d3775a09997d34ceacea1190936922
Deywerson Pereira <deywerson.pereira@gmail.com>
2025-04-30 20:49:55 -0300

    feat: added motivational phrase to send to someone special

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <h1>
+      Meu gatinho, você é muito especial e importante pra mim.
+      <br />
+      Eu te amo mil milhões *-*
+      <br />
+      Parabéns por ser esse homem incrível!
+    </h1>
+  );
 }
 
 export default Home;
